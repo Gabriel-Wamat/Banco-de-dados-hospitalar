c878ed301ab6b531e8176c04b02094ecdf4d3c2c
Gabriel W.A Matias <83626878+Gabriel-Wamat@users.noreply.github.com>
2024-09-04 19:48:03 -0300

    Update Consultas - GDI.sql

--- 3. Modelo_Físico/Consultas - GDI.sql
+++ 3. Modelo_Físico/Consultas - GDI.sql
@@ -100,8 +100,7 @@ WHERE CRM IN (
 -- Projete o nome do medico, nome do paciente atendido e o medicamento receitado
 SELECT M.Nome AS Nome_Medico,(SELECT P.Nome  
     				          FROM Paciente P 
-      				          WHERE P.CPF_Paciente = R.CPF_Paciente) AS Nome_Paciente, 
-                (SELECT MD.Nome  
+      				          WHERE P.CPF_Paciente = R.CPF_Paciente) AS Nome_Paciente, (SELECT MD.Nome  
     							  FROM Medicamento MD 
      							  WHERE MD.Cod = R.Cod_Medicamento) AS Nome_Medicamento 
 FROM Receita R JOIN Medico M ON R.CRM = M.CRM
